09b5f05809647340ee9bdf0a433e3070e384445e
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-14 11:57:00 -0700

    adds more validations to employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,6 @@
 class Employee < ApplicationRecord
   belongs_to :department
   validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, uniqueness: true
 end

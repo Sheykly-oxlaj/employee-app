df6a4bc4a29277534fdfec8ae7acf94dc437c431
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-14 11:10:26 -0700

    adds index, show and create action for employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,25 @@
 class EmployeesController < ApplicationController
+  def index 
+    employees = Employee.all
+    render json: employees.as_json
+  end 
+
+  def show
+    employee = Employee.find_by(id: params[:id])
+    render json: employee.as_json
+  end
+
+  def create
+    employee = Employee.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone: params["phone"],
+      job_title: params["job_title"],
+      department_id: params["department_id"]
+    )
+    employee.save
+    render json: employee.as_json
+  end 
+  
 end

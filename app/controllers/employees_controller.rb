5b34586ea5c48ebd21f98791f87ba50f35831502
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-14 11:40:30 -0700

    adds update and destroy methods

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -22,4 +22,22 @@ class EmployeesController < ApplicationController
     render json: employee.as_json
   end 
   
+  def update
+    employee = Employee.find_by(id: params[:id])
+    employee.first_name = params[:first_name] || employee.first_name
+    employee.last_name = params[:last_name] || employee.last_name
+    employee.email = params[:email] || employee.email
+    employee.phone = params[:phone] || employee.phone
+    employee.job_title = params[:job_title] || employee.job_title
+    employee.department_id = params[:department_id] || employee.department_id
+
+    employee.save 
+    render json: employee.as_json
+  end
+
+  def destroy
+    employee = Employee.find_by(id: params[:id])
+    employee.destroy
+    render json: {message: "employee removed succesfully"}
+  end
 end
